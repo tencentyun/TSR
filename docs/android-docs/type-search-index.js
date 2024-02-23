e3d5d12c6f77c66c7a0d8bc34d6a30cf30e8f007
jeffgaogao <jeffgaogao@tencent.com>
2024-02-23 17:55:13 +0800

    Update API documents

--- docs/android-docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.tencent.mps.tsr.api","l":"TsrSdk.SdkLicenseStatus"},{"p":"com.tencent.mps.tsr.api","l":"TsrLogger"},{"p":"com.tencent.mps.tsr.api","l":"TsrPass"},{"p":"com.tencent.mps.tsr.api","l":"TsrSdk"}];updateSearchResults();
